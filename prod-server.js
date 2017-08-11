bac67e8146312c82ea32ed985ec78bd03f6f34e3
simbikLev <simbik@live.ru>
2017-08-11 17:23:05 +0300

    Add favicon

--- prod-server.js
+++ prod-server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
 const root = path.join(__dirname, 'dist');
 app.use(fallback('index.html', { root }));
